83c053f4d580378006ff496e7051906fc57a1cb0
whats0n <mrdvv@mail.ru>
2015-06-02 12:30:29 +0300

    fixed

--- js/common.js
+++ js/common.js
@@ -167,4 +167,11 @@ head.ready(function() {
 		return false;
 
 	});
+
+	//only numbers
+	$('.js-numbers').bind("change keyup input click", function() {
+	    if (this.value.match(/[^0-9]/g)) {
+	        this.value = this.value.replace(/[^0-9]/g, '');
+	    }
+	});
 });
